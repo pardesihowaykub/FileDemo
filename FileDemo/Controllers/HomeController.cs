8640686003527d3ca3a8f58b020c922d8222d5d6
wasim <m_wasimrandhawa@hotmail.com>
2016-06-25 18:30:19 +0200

    filedemo

---
+++ FileDemo/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FileDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Upload()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Upload(string baseData)
+        {
+            if (HttpContext.Request.Files.AllKeys.Any())
+            {
+                for (int i = 0; i <= HttpContext.Request.Files.Count; i++)
+                {
+                    var file = HttpContext.Request.Files["files" + i];
+                    if (file != null)
+                    {
+                        var fileSavePath = Path.Combine(Server.MapPath("/Files"), file.FileName);
+                        file.SaveAs(fileSavePath);
+                    }
+                }
+            }
+            return View();
+        }
+
+        public ActionResult Download()
+        {
+            string[] files = Directory.GetFiles(Server.MapPath("/Files"));
+            for (int i = 0; i < files.Length; i++)
+            {
+                files[i] = Path.GetFileName(files[i]);
+            }
+            ViewBag.Files = files;
+            return View();
+        }
+
+        public FileResult DownloadFile(string fileName)
+        {
+            var filepath = System.IO.Path.Combine(Server.MapPath("/Files/"), fileName);
+            return File(filepath, MimeMapping.GetMimeMapping(filepath), fileName);
+        }
+    }
+}
